a96c6c633a8cbb50ae9ec6cc1d5de0593b54fd41
suhailkv <suhailkv99@gmail.com>
2020-09-29 17:21:14 +0530

    new coarse

---
+++ Building Web Applications in PHP/week5/index.php
@@ -0,0 +1,19 @@
+<title>Suhail's PHP</title>
+<h1>Suhail's Home</h1>
+<p><?php
+print "The SHA256 hash of \"Suhail\" is ".hash('sha256','Suhail')
+ ?></p>
+ <pre>
+ASCII ART:
+
+    ######
+    #    #
+    #
+    ######
+         #
+    #    #
+    ######
+  </pre>
+  <a href="fail.php">Click here to check the error setting</a>
+  <br>
+  <a href="check.php">Click here to cause a traceback</a>
